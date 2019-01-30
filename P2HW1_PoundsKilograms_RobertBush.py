71046a447c7591661248ce87b7c0acf5d9f17805
ven0mrb <47165311+ven0mrb@users.noreply.github.com>
2019-01-29 21:11:21 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_RobertBush.py
@@ -0,0 +1,16 @@
+# A brief description of the project
+# 1-28-2019
+# CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Robert Bush
+#
+
+#kg= lb/2.2046 as instructed on the blackboard assigment
+
+# Enter the Pounds (lb)
+pounds = float(input('Enter the pound(s):'))
+
+# Converting
+kilograms = pounds *2.2046
+
+#Converted to KG
+print('The Kilograms is', kilograms)
