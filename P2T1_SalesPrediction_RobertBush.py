62ce0f9daa86889276035af5dc05b1cf1a49e987
ven0mrb <47165311+ven0mrb@users.noreply.github.com>
2019-01-29 21:09:16 -0500

    Add files via upload
    Sale Prediction (Python)

---
+++ P2T1_SalesPrediction_RobertBush.py
@@ -0,0 +1,21 @@
+# P2T1- Sales Prediction - Programming Exercises
+# 1-19-2019
+# CTI-110 P2T1 - Sales Prediction
+# Robert Bush
+#
+print('Robert Bush')
+print('106 Cascade Circle')
+print('Dunn, NC 28334')
+print('704-275-1233')
+print('Information Technology')
+
+# Amount of total sales
+total_sales = float(input('What is the total of sales:'))
+
+# Profit 23% to .23
+profit = 0.23
+
+# Calculation profit from sales
+total_profit = total_sales*profit
+print('Total profit sales is')
+print(total_profit)
