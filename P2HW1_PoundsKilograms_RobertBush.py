72695005378df22bfac496e4e2ec07848dc965a7
ven0mrb <47165311+ven0mrb@users.noreply.github.com>
2019-02-24 10:13:26 -0500

    Update P2HW1_PoundsKilograms_RobertBush.py

--- P2HW1_PoundsKilograms_RobertBush.py
+++ P2HW1_PoundsKilograms_RobertBush.py
@@ -6,11 +6,14 @@
 
 #kg= lb/2.2046 as instructed on the blackboard assigment
 
-# Enter the Pounds (lb)
+# Enter the Pound(s) (lb) 
+# To make an input for the pound(s) to be entered
 pounds = float(input('Enter the pound(s):'))
 
 # Converting
+# After the pound(s) was entered, it is converted to Kilograms by the math opreation automatic  
 kilograms = pounds *2.2046
 
 #Converted to KG
+# To show the kilogram as it's final that is converted from the pound(s).
 print('The Kilograms is', kilograms)
